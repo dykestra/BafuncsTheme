02e0f7f0b5b680df3847010b6f77d8f88b1b1407
dykestra <na.k.bassett@gmail.com>
2017-08-11 11:20:36 +0100

    let text fill whole width if no image present

--- link-description.php
+++ link-description.php
@@ -1,12 +1,13 @@
 <div class="row link-description-row">
-    <div class="span7">
-        <figure class="featured-thumbnail thumbnail large">
-            <img src="<?php echo get_field('image'); ?>"/>
-        </figure>
-        <div class="clear"></div>
-    </div>
-
-    <div class="entry-content span5">
+    <?php $spansize = 12; if (get_field('image')): $spansize = 5; ?>
+        <div class="span7">
+            <figure class="featured-thumbnail thumbnail large">
+                <img src="<?php echo get_field('image'); ?>"/>
+            </figure>
+            <div class="clear"></div>
+        </div>
+    <?php endif ?>
+    <div class="entry-content span<?php echo $spansize; ?>">
         <div class="link-description">
             <?php echo get_field('description'); ?>
             <a class="btn btn-primary grey-bg-hover" href="<?php echo get_field('hyperlink'); ?>">
